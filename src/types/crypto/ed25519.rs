6cad3dce6f30a3a79b7d23e1ce03d7cd4396c765
Brandon Williams <brandon@mystenlabs.com>
2024-02-23 19:31:52 -0600

    ed25519: add utility methods

--- src/types/crypto/ed25519.rs
+++ src/types/crypto/ed25519.rs
@@ -34,6 +34,66 @@ pub struct Ed25519PublicKey(
 impl Ed25519PublicKey {
     /// The length of an ed25519 public key in bytes.
     pub const LENGTH: usize = 32;
+
+    pub const fn new(bytes: [u8; Self::LENGTH]) -> Self {
+        Self(bytes)
+    }
+
+    #[cfg(feature = "rand")]
+    #[cfg_attr(doc_cfg, doc(cfg(feature = "rand")))]
+    pub fn generate<R>(mut rng: R) -> Self
+    where
+        R: rand_core::RngCore + rand_core::CryptoRng,
+    {
+        let mut buf: [u8; Self::LENGTH] = [0; Self::LENGTH];
+        rng.fill_bytes(&mut buf);
+        Self::new(buf)
+    }
+
+    /// Return the underlying byte array of an Ed25519PublicKey.
+    pub const fn into_inner(self) -> [u8; Self::LENGTH] {
+        self.0
+    }
+
+    pub const fn inner(&self) -> &[u8; Self::LENGTH] {
+        &self.0
+    }
+
+    pub const fn as_bytes(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl std::str::FromStr for Ed25519PublicKey {
+    type Err = base64ct::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        super::Base64FromStr32::from_str(s).map(|a| Self::new(a.0))
+    }
+}
+
+impl AsRef<[u8]> for Ed25519PublicKey {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl AsRef<[u8; Self::LENGTH]> for Ed25519PublicKey {
+    fn as_ref(&self) -> &[u8; Self::LENGTH] {
+        &self.0
+    }
+}
+
+impl From<Ed25519PublicKey> for [u8; Ed25519PublicKey::LENGTH] {
+    fn from(public_key: Ed25519PublicKey) -> Self {
+        public_key.into_inner()
+    }
+}
+
+impl From<[u8; Self::LENGTH]> for Ed25519PublicKey {
+    fn from(public_key: [u8; Self::LENGTH]) -> Self {
+        Self::new(public_key)
+    }
 }
 
 impl std::fmt::Display for Ed25519PublicKey {
@@ -68,6 +128,66 @@ pub struct Ed25519Signature(
 impl Ed25519Signature {
     /// The length of an ed25519 signature key in bytes.
     pub const LENGTH: usize = 64;
+
+    pub const fn new(bytes: [u8; Self::LENGTH]) -> Self {
+        Self(bytes)
+    }
+
+    #[cfg(feature = "rand")]
+    #[cfg_attr(doc_cfg, doc(cfg(feature = "rand")))]
+    pub fn generate<R>(mut rng: R) -> Self
+    where
+        R: rand_core::RngCore + rand_core::CryptoRng,
+    {
+        let mut buf: [u8; Self::LENGTH] = [0; Self::LENGTH];
+        rng.fill_bytes(&mut buf);
+        Self::new(buf)
+    }
+
+    /// Return the underlying byte array of an Ed25519Signature.
+    pub const fn into_inner(self) -> [u8; Self::LENGTH] {
+        self.0
+    }
+
+    pub const fn inner(&self) -> &[u8; Self::LENGTH] {
+        &self.0
+    }
+
+    pub const fn as_bytes(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl std::str::FromStr for Ed25519Signature {
+    type Err = base64ct::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        super::Base64FromStr64::from_str(s).map(|a| Self::new(a.0))
+    }
+}
+
+impl AsRef<[u8]> for Ed25519Signature {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl AsRef<[u8; Self::LENGTH]> for Ed25519Signature {
+    fn as_ref(&self) -> &[u8; Self::LENGTH] {
+        &self.0
+    }
+}
+
+impl From<Ed25519Signature> for [u8; Ed25519Signature::LENGTH] {
+    fn from(signature: Ed25519Signature) -> Self {
+        signature.into_inner()
+    }
+}
+
+impl From<[u8; Self::LENGTH]> for Ed25519Signature {
+    fn from(signature: [u8; Self::LENGTH]) -> Self {
+        Self::new(signature)
+    }
 }
 
 impl std::fmt::Display for Ed25519Signature {
