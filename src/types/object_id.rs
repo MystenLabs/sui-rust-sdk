789842435ac39b584a66777c310da9d960836f5d
Brandon Williams <brandon@mystenlabs.com>
2024-04-12 07:23:23 -0500

    object_id: add a few conversion impls

--- src/types/object_id.rs
+++ src/types/object_id.rs
@@ -1,4 +1,26 @@
 use super::Address;
 
-#[derive(Eq, PartialEq, Clone, Copy, PartialOrd, Ord, Hash)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde_derive::Serialize, serde_derive::Deserialize)
+)]
+#[cfg_attr(test, derive(proptest_derive::Arbitrary))]
 pub struct ObjectId(Address);
+
+impl ObjectId {
+    pub const LENGTH: usize = Address::LENGTH;
+    pub const ZERO: Self = Self(Address::ZERO);
+}
+
+impl From<Address> for ObjectId {
+    fn from(value: Address) -> Self {
+        Self(value)
+    }
+}
+
+impl From<ObjectId> for Vec<u8> {
+    fn from(value: ObjectId) -> Self {
+        value.0.into()
+    }
+}
