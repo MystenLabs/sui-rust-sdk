c6718da05e461a12ffd13a533da395b5a24dc4c6
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2024-09-18 21:14:39 -0700

    Add staging

--- crates/sui-graphql-client/build.rs
+++ crates/sui-graphql-client/build.rs
@@ -1,5 +1,10 @@
 /// Register Sui RPC schema for creating structs for queries
 fn main() {
+    #[cfg(feature = "staging")]
+    cynic_codegen::register_schema("rpc")
+        .from_sdl_file("schema/graphql_rpc_staging.graphql")
+        .expect("Failed to find GraphQL Schema");
+    #[cfg(not(feature = "staging"))]
     cynic_codegen::register_schema("rpc")
         .from_sdl_file("schema/graphql_rpc.graphql")
         .expect("Failed to find GraphQL Schema");
