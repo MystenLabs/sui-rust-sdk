d3699d791b0c56e0e540ef65f166ecdc0a9c9cf5
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2024-10-17 21:46:02 -0700

    Add tests

--- crates/sui-transaction-builder/src/lib.rs
+++ crates/sui-transaction-builder/src/lib.rs
@@ -36,13 +36,22 @@ use serde::Serialize;
 
 #[derive(Clone, Debug)]
 pub struct TransactionBuilder {
+    /// The inputs to the transaction.
     inputs: Vec<Input>,
+    /// The list of commands in the transaction. A command is a single operation in a programmable
+    /// transaction.
     commands: Vec<Command>,
+    /// The gas objects that will be used to pay for the transaction.
     gas: Vec<Object>,
+    /// The gas budget for the transaction.
     gas_budget: u64,
+    /// The gas price for the transaction.
     gas_price: u64,
+    /// The sender of the transaction.
     sender: Address,
+    /// The sponsor of the transaction. If None, the sender is also the sponsor.
     sponsor: Option<Address>,
+    /// The expiration of the transaction.
     expiration: Option<TransactionExpiration>,
 }
 
@@ -97,7 +106,7 @@ impl Function {
 
 /// A transaction state that can be resolved or unresolved. When calling [`try_finish`]` on the
 /// transaction builder, it will return either the resolved transaction if no error was
-/// encountered, or the Unresolved. The unresolved transaction can be used to call the transaction
+/// encountered, or the [`UnresolvedTransaction`]. The unresolved transaction can be used to call the transaction
 /// resolution API, that will attempt to resolve the transaction.
 pub enum TransactionResolution {
     Resolved(Transaction),
@@ -117,7 +126,9 @@ impl Value {
     }
 }
 
+/// A transaction builder to build transactions.
 impl TransactionBuilder {
+    /// Create a new transaction builder and initialize its elements to default.
     pub fn new() -> Self {
         Self {
             inputs: Vec::new(),
@@ -142,31 +153,38 @@ impl TransactionBuilder {
 
     // Metadata
 
+    /// Set the gas objects that will be used to pay for the transaction.
     pub fn set_gas(&mut self, gas: Vec<Object>) {
         self.gas = gas;
     }
 
+    /// Set the gas budget for the transaction.
     pub fn set_gas_budget(&mut self, budget: u64) {
         self.gas_budget = budget;
     }
 
+    /// Set the gas price for the transaction.
     pub fn set_gas_price(&mut self, price: u64) {
         self.gas_price = price;
     }
 
+    /// Set the sender of the transaction.
     pub fn set_sender(&mut self, sender: Address) {
         self.sender = sender;
     }
 
+    /// Set the sponsor of the transaction.
     pub fn set_sponsor(&mut self, sponsor: Address) {
         self.sponsor = Some(sponsor);
     }
 
+    /// Set the expiration of the transaction.
     pub fn set_expiration(&mut self, epoch: u64) {
         self.expiration = Some(TransactionExpiration::Epoch(epoch));
     }
 
     // Commands
+    /// Call a Move function with the given arguments.
     pub fn move_call(&mut self, function: Function, arguments: Vec<Value>) -> Value {
         let cmd = Command::MoveCall(MoveCall {
             arguments: arguments.into_iter().map(|a| a.into()).collect(),
@@ -199,9 +217,9 @@ impl TransactionBuilder {
     }
 
     /// Merge a list of coins into a single coin.
-    pub fn merge_coins(&mut self, into: Value, coins: Vec<Value>) {
+    pub fn merge_coins(&mut self, into_coin: Value, coins: Vec<Value>) {
         let cmd = Command::MergeCoins(MergeCoins {
-            coin: into.into(),
+            coin: into_coin.into(),
             coins_to_merge: coins.into_iter().map(|c| c.into()).collect(),
         });
         self.commands.push(cmd);
@@ -216,7 +234,7 @@ impl TransactionBuilder {
         self.commands.push(cmd);
     }
 
-    /// Publish a new module.
+    /// Publish a list of modules with the given dependencies.
     pub fn publish(&mut self, modules: Vec<Vec<u8>>, dependencies: Vec<ObjectId>) -> Value {
         let cmd = Command::Publish(Publish {
             modules,
@@ -367,7 +385,7 @@ impl From<Value> for Input {
     fn from(value: Value) -> Input {
         match value {
             Value::Input(i) => Input::Pure(i.to_be_bytes().to_vec()),
-            _ => todo!(),
+            _ => panic!("Cannot convert Value to Input"),
         }
     }
 }
@@ -485,5 +503,149 @@ impl From<Input> for UnresolvedInputArgument {
                 mutable: None,
             },
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::anyhow;
+    use base64ct::Encoding;
+    use std::str::FromStr;
+    use sui_crypto::ed25519::Ed25519PrivateKey;
+    use sui_crypto::SuiSigner;
+    use sui_graphql_client::Client;
+
+    use sui_graphql_client::faucet::FaucetClient;
+    use sui_types::types::{Address, ObjectDigest, ObjectId};
+
+    use crate::object::Object;
+    use crate::{Serialized, TransactionBuilder};
+
+    #[tokio::test]
+    async fn test_tx_finish() {
+        let mut tx = TransactionBuilder::new();
+        let coin_obj_id = "0x19406ea4d9609cd9422b85e6bf2486908f790b778c757aff805241f3f609f9b4";
+        let coin_digest = "7opR9rFUYivSTqoJHvFb9p6p54THyHTatMG6id4JKZR9";
+        let coin_version = 2;
+        let coin = tx.input(Object::owned(
+            ObjectId::from_str(coin_obj_id).unwrap(),
+            coin_version,
+            ObjectDigest::from_str(coin_digest)
+                .map_err(|_| anyhow!("Invalid object digest"))
+                .unwrap(),
+        ));
+
+        let recipient = tx.input(Serialized(
+            &Address::from_str(
+                "0xeeaeb20e7b3a9cfce768a2ddf69503d8ae8d1628a26c2d74a435501c929d3f69",
+            )
+            .map_err(|_| anyhow!("Invalid address"))
+            .unwrap(),
+        ));
+
+        assert!(tx.finish().is_err());
+
+        tx.transfer_objects(vec![coin], recipient);
+        tx.set_gas_budget(500000000);
+        tx.set_gas_price(1000);
+        tx.set_gas(vec![Object::owned(
+            ObjectId::from_str(
+                "0xd8792bce2743e002673752902c0e7348dfffd78638cb5367b0b85857bceb9821",
+            )
+            .unwrap(),
+            2,
+            ObjectDigest::from_str("2ZigdvsZn5BMeszscPQZq9z8ebnS2FpmAuRbAi9ednCk")
+                .map_err(|_| anyhow!("Invalid object digest"))
+                .unwrap(),
+        )]);
+        tx.set_sender(
+            Address::from_str("0xc574ea804d9c1a27c886312e96c0e2c9cfd71923ebaeb3000d04b5e65fca2793")
+                .unwrap(),
+        );
+
+        let tx = tx.finish();
+        assert!(tx.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_try_finish() {
+        let mut tx = TransactionBuilder::new();
+        let coin_obj_id = "0x19406ea4d9609cd9422b85e6bf2486908f790b778c757aff805241f3f609f9b4";
+        let coin_digest = "7opR9rFUYivSTqoJHvFb9p6p54THyHTatMG6id4JKZR9";
+        let coin_version = 2;
+        let _coin = tx.input(Object::owned(
+            ObjectId::from_str(coin_obj_id).unwrap(),
+            coin_version,
+            ObjectDigest::from_str(coin_digest)
+                .map_err(|_| anyhow!("Invalid object digest"))
+                .unwrap(),
+        ));
+
+        let tx = tx.try_finish();
+        assert!(tx.is_ok());
+        assert!(matches!(
+            tx.unwrap(),
+            crate::TransactionResolution::Unresolved(_)
+        ));
+    }
+
+    /// Test TransferObj PTB.
+    #[tokio::test]
+    async fn test_transfer_obj_execution() {
+        let pk_bcs =
+            base64ct::Base64::decode_vec("AAjgPs/zbxDsObju6Tp2/8W5lNWP/sjUDsVxR1vgdmyT").unwrap();
+        let pk = Ed25519PrivateKey::new(pk_bcs[1..].try_into().expect("slice has wrong length"));
+        let address = pk.public_key().to_address();
+        // request coins from local network faucet
+        let coins = FaucetClient::local()
+            .request_and_wait(address)
+            .await
+            .unwrap()
+            .unwrap()
+            .sent;
+        let first = coins.first().unwrap().id;
+        let gas = coins.last().unwrap().id;
+
+        let mut tx = TransactionBuilder::new();
+
+        // get the object information from the client
+        let client = Client::new_localhost();
+        let coin = client.object(first.into(), None).await.unwrap().unwrap();
+        let coin_digest = coin.digest();
+        let coin_version = coin.version();
+
+        let coin_input = tx.input(Object::owned(coin.object_id(), coin_version, coin_digest));
+        let recipient = Address::generate(rand::thread_rng());
+        let recipient_input = tx.input(Serialized(&recipient));
+        tx.transfer_objects(vec![coin_input], recipient_input);
+        tx.set_gas_budget(500000000);
+        tx.set_gas_price(1000);
+
+        let gas_obj = client.object(gas.into(), None).await.unwrap().unwrap();
+        tx.set_gas(vec![Object::owned(gas, 2, gas_obj.digest())]);
+
+        tx.set_sender(address);
+        let tx = tx.finish().unwrap();
+        let sig = pk.sign_transaction(&tx).unwrap();
+
+        let effects = client.execute_tx(vec![sig], &tx).await;
+        println!("{:?}", &effects);
+        assert!(effects.is_ok());
+        assert!(effects.unwrap().is_some());
+
+        // wait for the transaction to be finalized
+        loop {
+            let tx_digest = client.transaction(&tx.digest().to_base58()).await.unwrap();
+            if tx_digest.is_some() {
+                break;
+            }
+        }
+
+        // check that recipient has 1 coin
+        let recipient_coins = client
+            .coins(recipient, None, None, None, None, None)
+            .await
+            .unwrap();
+        assert_eq!(recipient_coins.unwrap().data().len(), 1);
     }
 }
