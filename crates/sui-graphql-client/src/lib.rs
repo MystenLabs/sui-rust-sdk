b633e0b7fb330ce00d730e951925046defa54afc
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2024-10-17 18:07:22 -0700

    Make it a constant

--- crates/sui-graphql-client/src/lib.rs
+++ crates/sui-graphql-client/src/lib.rs
@@ -71,6 +71,7 @@ use futures::Stream;
 use reqwest::Url;
 use std::pin::Pin;
 
+const ITEMS_PER_PAGE: i32 = 10;
 const MAINNET_HOST: &str = "https://sui-mainnet.mystenlabs.com/graphql";
 const TESTNET_HOST: &str = "https://sui-testnet.mystenlabs.com/graphql";
 const DEVNET_HOST: &str = "https://sui-devnet.mystenlabs.com/graphql";
@@ -125,7 +126,7 @@ impl Default for PaginationFilter<'_> {
         Self {
             direction: Direction::Forward,
             cursor: None,
-            limit: Some(10),
+            limit: Some(ITEMS_PER_PAGE),
         }
     }
 }
