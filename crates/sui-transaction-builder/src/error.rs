1fca6bf880648f034c3a8abc43dab3e1a4351f02
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2024-12-02 14:37:16 -0800

    Address feedback on errors

--- crates/sui-transaction-builder/src/error.rs
+++ crates/sui-transaction-builder/src/error.rs
@@ -13,41 +13,28 @@ pub enum Error {
     WrongGasObject,
     #[error("Decoding error: {0}")]
     Decoding(#[from] Base64Error),
-    /// Missing object id
     #[error("Missing object id")]
     MissingObjectId,
-    /// Missing version for the respective object
     #[error("Missing version for object {0}")]
     MissingVersion(ObjectId),
-    /// Missing digest for the respective object
     #[error("Missing digest for object {0}")]
     MissingDigest(ObjectId),
-    /// Missing the sender for this transaction. A sender is always required when building a
-    /// transaction.
     #[error("Missing sender")]
     MissingSender,
-    /// Missing the gas objects for this transaction
     #[error("Missing gas objects")]
     MissingGasObjects,
-    /// Missing the gas budget for this transaction
     #[error("Missing gas budget")]
     MissingGasBudget,
-    /// Missing the gas price for this transaction
     #[error("Missing gas price")]
     MissingGasPrice,
-    /// Missing the object kind for this object
     #[error("Missing object kind for object {0}")]
     MissingObjectKind(ObjectId),
-    /// Missing the initial shared version for this object
     #[error("Missing initial shared version for object {0}")]
     MissingInitialSharedVersion(ObjectId),
-    /// Missing the value data for this input that is of type Pure
     #[error("Missing pure value")]
     MissingPureValue,
-    /// Cannot determine the shared object mutability for this object
     #[error("Unknown shared object mutability for object {0}")]
     SharedObjectMutability(ObjectId),
-    /// Literals are not yet supported.
     #[error("Unsupported literal")]
     UnsupportedLiteral,
 }
