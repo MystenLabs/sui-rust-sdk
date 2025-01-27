b4624cbf1be92880ab2bd9cdb6933d55732a77cd
Brandon Williams <brandon@mystenlabs.com>
2025-01-27 16:28:46 -0600

    WIP

--- crates/sui-sdk-types/src/lib.rs
+++ crates/sui-sdk-types/src/lib.rs
@@ -327,3 +327,8 @@ mod _serde {
 
     pub(crate) use super::SignedTransactionWithIntentMessage;
 }
+
+// declare `roaring` as a public dependency (for the moment) since its currently exposed in a few
+// types
+#[doc(hidden)]
+pub use roaring;
