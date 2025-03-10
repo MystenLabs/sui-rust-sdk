3996ecc7b95457ac962cc4d54bf0691018b1b1a7
bmwill <bmwill@users.noreply.github.com>
2025-03-10 13:52:31 +0000

    deploy: 12b6e9567c716e113b3ca561f2952eab5a768c99

---
+++ trait.impl/cynic/schema/trait.MutationRoot.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sui_graphql_client",[["impl MutationRoot for <a class=\"struct\" href=\"sui_graphql_client/query_types/schema/struct.Mutation.html\" title=\"struct sui_graphql_client::query_types::schema::Mutation\">Mutation</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[218]}
