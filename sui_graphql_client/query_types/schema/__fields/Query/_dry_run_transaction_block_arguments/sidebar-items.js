0efb237cbab956bb97c57d56871a6bd7cba2288d
bmwill <bmwill@users.noreply.github.com>
2025-08-18 13:31:18 +0000

    deploy: ceeee4a30cdd1da23e48d43040459933ade7e691

---
+++ sui_graphql_client/query_types/schema/__fields/Query/_dry_run_transaction_block_arguments/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["skipChecks","txBytes","txMeta"]};
---
+++ sui_graphql_client/query_types/schema/__fields/RandomnessStateUpdateTransaction/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["__typename","epoch","randomBytes","randomnessObjInitialSharedVersion","randomnessRound"]};
