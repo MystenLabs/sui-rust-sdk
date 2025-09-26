a906501749691c6f84989692d2f5e73a8d3d5d4a
Nick <nickvikeras@gmail.com>
2025-09-26 15:56:07 -0500

    move select_n to sdk

--- crates/sui-rpc/src/client/v2/coin_selection.rs
+++ crates/sui-rpc/src/client/v2/coin_selection.rs
@@ -50,5 +50,38 @@ impl Client {
             "Insufficient funds for address [{address}], requested amount: {amount}, total available: {total}",
         )))
     }
-}
 
+    pub async fn select_up_to_n_largest_coins(
+        &self,
+        address: &str,
+        coin_type: &str,
+        n: usize,
+    ) -> Result<Vec<Object>> {
+        let mut selected_coins = vec![];
+
+        let list_request = ListOwnedObjectsRequest::default()
+            .with_owner(address)
+            .with_object_type(format!("0x2::coin::Coin<{coin_type}>"))
+            .with_page_size(500u32)
+            .with_read_mask(FieldMask::from_paths([
+                "object_id",
+                "version",
+                "digest",
+                "balance",
+            ]));
+
+        let mut coin_stream = Box::pin(self.list_owned_objects(list_request));
+
+        while selected_coins.len() < n {
+            match coin_stream.next().await {
+                Some(Ok(object)) => {
+                    selected_coins.push(object);
+                }
+                Some(Err(e)) => return Err(e),
+                None => break,
+            }
+        }
+
+        Ok(selected_coins)
+    }
+}
