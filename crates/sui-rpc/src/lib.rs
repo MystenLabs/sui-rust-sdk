9fa5d7beda3083e3e097fca994de84635ebecd5d
Brandon Williams <brandon@mystenlabs.com>
2025-10-03 14:37:43 -0500

    sui-rpc: move FaucetClient from sui-grphql-client crate

--- crates/sui-rpc/src/lib.rs
+++ crates/sui-rpc/src/lib.rs
@@ -1,8 +1,14 @@
+#![cfg_attr(doc_cfg, feature(doc_cfg))]
+
 pub mod client;
 pub mod field;
 pub mod headers;
 pub mod merge;
 pub mod proto;
+
+#[cfg(feature = "faucet")]
+#[cfg_attr(doc_cfg, doc(cfg(feature = "faucet")))]
+pub mod faucet;
 
 pub use client::Client;
 
