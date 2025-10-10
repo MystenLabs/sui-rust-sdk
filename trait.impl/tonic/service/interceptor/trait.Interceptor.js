2bbf1c350facfe53ecba5077b682fbb409c5e399
bmwill <bmwill@users.noreply.github.com>
2025-10-10 16:55:44 +0000

    deploy: ec1547f12b53eca84e509c639c069bc01a1a4a76

---
+++ trait.impl/tonic/service/interceptor/trait.Interceptor.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sui_rpc",[["impl Interceptor for &amp;mut <a class=\"struct\" href=\"sui_rpc/client/struct.AuthInterceptor.html\" title=\"struct sui_rpc::client::AuthInterceptor\">AuthInterceptor</a>"],["impl Interceptor for <a class=\"struct\" href=\"sui_rpc/client/struct.AuthInterceptor.html\" title=\"struct sui_rpc::client::AuthInterceptor\">AuthInterceptor</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[356]}
