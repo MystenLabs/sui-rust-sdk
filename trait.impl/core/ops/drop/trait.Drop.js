078f02c93660fe70bfcc4bd4a2e9b782cbb9652e
bmwill <bmwill@users.noreply.github.com>
2026-04-13 21:04:54 +0000

    deploy: 603cd236a69fdfa0fa9737b6d086dadf6c995db1

---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["integration_tests",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"integration_tests/struct.SuiNetworkHandle.html\" title=\"struct integration_tests::SuiNetworkHandle\">SuiNetworkHandle</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[326]}
